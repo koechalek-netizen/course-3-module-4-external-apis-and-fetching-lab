a171f39971b6d01e9ecbfc737d71a6645e92714e
alekkoech <koechalek@gmail.com>
2026-04-13 16:01:31 +0300

    updated the code correctly for the API lab

--- index.js
+++ index.js
@@ -2,52 +2,95 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
-function fetchWeatherAlerts(state) {
-    const url = "https://api.weather.gov/alerts/active?area= ${state}"
+// function fetchWeatherAlerts(state) {
+    
 
-    fetch(url)
-    .then(response => {
-        if(!response.ok){
-            throw new Error ('Network response was not ok')
-        }
-        return response.json()
-    })
-    .then(data =>{
-        console.log(data)
-        dislayAlerts(data)
-    })
-    .catch(errorObject =>{
-        handleError(errorObject.message)
-    })
+//     fetch(`https://api.weather.gov/alerts/active?area=${STATE_ABBR}`)
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error ('Network response was not ok')
+//         }
+//         return response.json()
+//     })
+//     .then(data =>{
+//         console.log(data)
+//         displayAlerts(data)
+//     })
+//     .catch(errorObject =>{
+//         handleError(errorObject.message)
+//     })
+// }
+
+// function displayAlerts(data){
+//     const alertsContainer = document.getElementById('alerts-container')
+//     const summaryHeader = document.getElementById('summary-header')
+
+//     alertsContainer.innerHTML = '';
+//     hideError()
+
+//     const title = data.title;
+//     const count = data.features.length
+//     summaryHeader.textContent = `${title}: ${count}`
+
+//     data.features.forEach(alert=>{
+//         const headline = alert.properties.headline;
+//         const li = document.createElement('li')
+//         li.textContent = headline
+//         alertsContainer.appendChild()
+//     })
+
+//     function handleError(message){
+//         const errorDiv = document.getElementById('error-message')
+//         errorDiv.textContent = message
+//         errorDiv.style.display = 'block'
+//     }
+
+//     function hideError(){
+//         const errorDiv = document.getElementById('error-message')
+//         errorDiv.textContent= ''
+//         errorDiv.style.display= 'none'
+//   
+// CORRECT CODE
+
+async function fetchWeatherAlerts(state) {
+  const input = document.getElementById("state-input")
+  const errorDiv = document.getElementById("error-message")
+
+  try {
+    const response = await fetch(weatherApi + state)
+    const data = await response.json()
+
+    displayAlerts(data)
+
+    input.value = ""
+
+  } catch (error) {
+    errorDiv.textContent = error.message
+    errorDiv.classList.remove("hidden")
+  }
 }
 
-function displayAlerts(data){
-    const alertsContainer = document.getElementById('alerts-container')
-    const summaryHeader = document.getElementById('summary-header')
+function displayAlerts(data) {
 
-    alertsContainer.innerHTML = '';
-    hideError()
+  const displayDiv = document.getElementById("alerts-display")
+  const errorDiv = document.getElementById("error-message")
 
-    const title = data.title;
-    const count = data.features.length
-    summaryHeader.textContent = `${title}: ${count}`
+  displayDiv.innerHTML = ""
+  errorDiv.textContent = ""
+  errorDiv.classList.add("hidden")
 
-    data.features.forEach(alert=>{
-        const headline = alert.properties.headline;
-        const li = document.createElement('li')
-        li.textContent = headline
-        alertsContainer.appendChild()
-    })
+  const alerts = data.features
 
-    function handleError(message){
-        const errorDiv = document.getElementById('error-message')
-        errorDiv.textContent = message
-        errorDiv.style.display = 'block'
-    }
+  displayDiv.textContent = `Weather Alerts: ${alerts.length}`
 
-    function hideError(){
-        const errorDiv = document.getElementById('error-message')
-        errorDiv.textContent= ''
-        errorDiv.style.display= 'none'
-    }
+  for (let alert of alerts) {
+    const p = document.createElement("p")
+    p.textContent = alert.properties.headline
+    displayDiv.appendChild(p)
+  }
 }
+
+document.getElementById("fetch-alerts").addEventListener("click", () => {
+  const state = document.getElementById("state-input").value
+  fetchWeatherAlerts(state)
+})
